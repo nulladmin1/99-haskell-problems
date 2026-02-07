df8f0e833e693041c02ae1cac8f6b0f74570af15
nulladmin1 <shrey.deogade@protonmail.com>
2026-02-07 15:36:20 -0500

    initial

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "cs2programs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    formatter = forEachSystem ({pkgs, ...}: pkgs.alejandra);
+
+    devShells = forEachSystem ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+          ]
+          ++ (with haskellPackages; [
+            haskell-language-server
+            cabal-install
+          ]);
+      };
+    });
+  };
+}
