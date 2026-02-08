dec7ffa5f7385e06f38af0e59407fdffa6d7d6d5
nulladmin1 <shrey.deogade@protonmail.com>
2026-02-08 14:30:19 -0500

    Finish P01

---
+++ src/P01.hs
@@ -0,0 +1,11 @@
+module P01 where
+
+import Data.List (uncons)
+
+myLast :: [a] -> Maybe a
+myLast [] = Nothing
+myLast xs = Just $ last xs
+
+myLast2 :: [a] -> Maybe a
+myLast2 [] = Nothing
+myLast2 xs = fmap fst $ uncons $ reverse xs
---
+++ test/Main.hs
@@ -0,0 +1,40 @@
+module Main (main) where
+
+import P01
+import qualified System.Exit as Exit
+import Test.HUnit
+
+p01 :: Test
+p01 =
+  TestLabel
+    "P01"
+    $ TestList
+      [ TestCase $
+          assertEqual
+            "myLast [1,2,3]"
+            (Just 3)
+            (myLast ([1, 2, 3] :: [Int])),
+        TestCase $
+          assertEqual
+            "myLast []"
+            Nothing
+            (myLast ([] :: [Int])),
+        TestCase $
+          assertEqual
+            "myLast2 []"
+            Nothing
+            (myLast2 ([] :: [Int])),
+        TestCase $
+          assertEqual
+            "myLast2 [1,2,3]"
+            (Just 3)
+            (myLast ([1, 2, 3] :: [Int]))
+      ]
+
+tests :: Test
+tests = TestList [p01]
+
+main :: IO ()
+main = do
+  c <- runTestTT tests
+  if failures c > 0 then Exit.exitFailure else Exit.exitSuccess
